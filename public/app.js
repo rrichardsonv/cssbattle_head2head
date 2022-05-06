24a3f18650a0bc5942565fab5bb923b138080c6e
Rich Richardson <rrichardsonv+github@gmail.com>
2022-05-06 16:27:05 -0500

    Minor refactoring

--- public/app.js
+++ public/app.js
@@ -1,53 +1,175 @@
-const socket = io();
-const players = {};
+class App {
+  constructor () {
+    // state
+    this.players = {};
+    this.lbp = {};
+    this.main = document.querySelector('main');
+    this.leaderBoard = document.querySelector('#leaderboard');
 
-socket.on('change', (payload) => {
-  const { name, text } = JSON.parse(payload);
+    // Network handlers
+    this.socket = new Socket();
+    this.socket.addListener(this.upsertPlayer.bind(this));
+    this.socket.addListener(this.upsertLeaderboard.bind(this));
+    this.socket.addListener(this.reorderLeaderboard.bind(this));
+    this.socket.addListener(this.updateSplitScreen.bind(this));
 
-  if (players[name] === undefined) {
-    players[name] = createPlayer(name, text);
-  } else {
-    players[name](text);
+    // UI events
+    this.bindCopyButton();
   }
-});
 
-function createPlayer (name, text) {
-  const d = document.createElement('div');
-  d.id = name.replace(/\s/, '-');
-  d.classList.add('player');
+  toggleCode () {
+    Object.keys(this.players).forEach(n => {
+      this.players[n].toggleSyntax()
+    })
+  }
 
-  const t = document.createElement('h3');
-  t.innerText = name;
-  d.appendChild(t);
+  upsertPlayer(p) {
+    const { name } = p;
+    this.players[name] ??= new PreviewPlayer(p, this.main);
+    this.players[name].update(p);
+    return p;
+  }
 
-  const p = document.createElement('iframe');
-  p.classList.add('preview');
-  p.title = "Preview";
-  p.style.background = "white";
-  p.style.width = "400px";
-  p.style.height = "300px";
-  p.style.border = "0";
-  p.style.outline = "0";
+  upsertLeaderboard(p) {
+    const {name} = p;
+    
+    this.lbp[name] ??= new ScorePlayer(p, this.leaderBoard);
+    this.lbp[name].update(p);
+    return p;
+  }
 
-  const c = document.createElement('div');
-  const code = document.createElement('code');
-  const pre = document.createElement('pre');
-
-  code.appendChild(pre);
-  c.appendChild(code);
-  d.appendChild(c);
-  d.appendChild(p);
-
-  document.body.appendChild(d);
+  reorderLeaderboard(unused) {
+    const rankedPlayers =
+      Object.keys(this.lbp).sort((a, b) =>
+        (this.lbp[a].count > this.lbp[b].count) ? 1 : (this.lbp[a].count > this.lbp[b].count) ? -1 : 0
+      ).map((k) => {
+        this.lbp[k].prepare();
+        return this.lbp[k];
+      });
   
+    this.leaderBoard.innerHTML = '';
+    
+    rankedPlayers.forEach(p => {
+      p.rerender(this.leaderBoard);
+    });
 
-  const update = (text) => {
-    pre.innerText = text;
-    p.contentDocument.write(text.replace(/^\s+/gm, '').replace(/\s+$/, ''));
-    p.contentWindow.document.close();
-  };
+    return unused;
+  }
+  
+  updateSplitScreen(unused) {
+    if (Object.keys(this.players).length > 3) {
+      this.main.classList.add('multi');
+    }
+    return unused;
+  }
 
-  update(text);
-  return update;
-};
+  bindCopyButton () {
+    const copyButtons = document.querySelectorAll("[data-copy-to-clipboard]");
+    if (copyButtons && copyButtons.length) {
+      copyButtons.forEach((btn) =>
+        btn.addEventListener("click", function (e) {
+          e.preventDefault();
+          const copyText = document.getElementById(btn.dataset.target);
 
+          document.execCommand("copy", false, copyText.select());
+        }),
+      );
+    }
+  }
+}
+
+class Socket {
+  constructor () {
+    this.socket = io();
+    this.socket.on('change', this.handleChange.bind(this));
+    this.listeners = [];
+  }
+
+  addListener (cb) {
+    this.listeners.push(cb);
+  }
+
+  handleChange (payload) {
+    pipe(
+      this.parsePayload,
+      this.cleanPayload,
+      ...this.listeners
+    )(payload);
+  }
+
+  parsePayload(payload) {
+    return JSON.parse(payload)
+  }
+
+  cleanPayload({ name, text }) {
+    return {
+      name,
+      id: name.replace(/\s/, '-'),
+      text: text.replace(/^\s*1\n/, '').replace(/(\S)\n\d+\n/g, '$1\n')
+    };
+  }
+}
+
+class PreviewPlayer {
+  constructor ({ name, id }, main) {
+    const el = document.createElement('div');
+    el.id = id;
+    el.classList.add('player');
+    el.innerHTML = `
+      <h3>${name}</h3>
+      <div id="syntax" class="hidden">
+        <code>
+          <pre class="syntax"></pre>
+        </code>
+      </div>
+      <iframe
+        title="Preview"
+        width="400"
+        height="300"
+        class="preview"
+        style=background:white;border:0;outline:0
+      ></iframe>
+    `;
+    this.el = el;
+    this.preview = el.querySelector('.preview');
+    this.pre = el.querySelector('.syntax');
+
+    main.appendChild(this.el);
+  }
+
+  update ({ text }) {
+    this.pre.innerText = text;
+    this.preview.contentDocument.write(text.replace(/^\s+/gm, '').replace(/\s+$/, ''))
+    this.preview.contentWindow.document.close();
+  }
+
+  toggleSyntax () {
+    this.el.getElementById('syntax').classList.toggle('hidden');
+  }
+}
+
+class ScorePlayer {
+  constructor ({ id, text }, leaderBoard) {
+    const el = document.createElement('p');
+    el.id = 'leaderboard-'+id;
+    this.el = el;
+    this.count = text.length;
+    leaderBoard.appendChild(this.el);
+  }
+  prepare () {
+    this.el = this.el.cloneNode(true);
+  }
+  rerender (leaderBoard) {
+    leaderBoard.appendChild(this.el);
+  }
+  update ({ name, text }) {
+    this.count = text.length;
+    this.el.innerText = `${name} - ${text.length}`;
+  }
+}
+
+function pipe (...fns) {
+  return (...rest) => fns.reduce((args, fn) => [fn(...args)], rest)[0];
+}
+
+window.app = new App();
--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const path = require("path");
-const http = require("http");
+const path = require('path');
+const http = require('http');
 
 // Require the fastify framework and instantiate it
-const fastify = require("fastify")({
+const fastify = require('fastify')({
   // set this to true for detailed logging:
   logger: false
 });
 
 
 // Setup our static files
-fastify.register(require("fastify-static"), {
-  root: path.join(__dirname, "public"),
-  prefix: "/" // optional: default '/'
+fastify.register(require('fastify-static'), {
+  root: path.join(__dirname, 'public'),
+  prefix: '/' // optional: default '/'
 });
 
 fastify.register(require('fastify-socket.io'), {
@@ -19,26 +19,28 @@ fastify.register(require('fastify-socket.io'), {
 })
 
 // fastify-formbody lets us parse incoming forms
-fastify.register(require("fastify-formbody"));
+fastify.register(require('fastify-formbody'));
 
 // point-of-view is a templating manager for fastify
-fastify.register(require("point-of-view"), {
+fastify.register(require('point-of-view'), {
   engine: {
-    handlebars: require("handlebars")
+    handlebars: require('handlebars')
   }
 });
 
 // Our main GET home page route, pulls from src/pages/index.hbs
-fastify.get("/", function(request, reply) {
+fastify.get('/', function(request, reply) {
   // params is an object we'll pass to our handlebars template
+  const joinScript = buildJoinScript(process.env.URL || 'http://localhost:3000');
   let params = {
-    server_url: process.env.URL || "http://localhost:3000"
+    joinScript,
+    joinScriptMin: joinScript.replace(/\n\s+/g, ' ')
   };
   // request.query.paramName <-- a querystring example
-  reply.view("/src/pages/index.hbs", params);
+  reply.view('/src/pages/index.hbs', params);
 });
 
-fastify.post("/event", function (request, reply) {
+fastify.post('/event', function (request, reply) {
   fastify.io.emit('change', request.body);
 
   reply.send();
@@ -47,12 +49,12 @@ fastify.post("/event", function (request, reply) {
 
 
 // A POST route to handle form submissions
-fastify.post("/", function(request, reply) {
+fastify.post('/', function(request, reply) {
   let params = {
-    greeting: "Hello Form!"
+    greeting: 'Hello Form!'
   };
   // request.body.paramName <-- a form post example
-  reply.view("/src/pages/index.hbs", params);
+  reply.view('/src/pages/index.hbs', params);
 });
 
 fastify.ready(err => {
@@ -80,3 +82,98 @@ fastify.listen(3000, function(err, address) {
   console.log(`Your app is listening on ${address}`);
   fastify.log.info(`server listening on ${address}`);
 });
+
+
+function buildJoinScript (serverUrl) {
+  return `(() => {
+    function quickH (obj) {
+      return ((str, hash = 5381, i = null) => {
+        while (i ??= str.length) {
+          hash = (hash * 33) ^ str.charCodeAt(--i);
+        };
+        return hash >>> 0;
+      })(JSON.stringify(obj));
+    }
+
+    let
+      lastHash = -1,
+      debug = false;
+
+    const
+      name = prompt('Enter your name to connect'),
+      editor = document.querySelector('.react-codemirror2');
+    
+    function req (payload) {
+      return {
+        method: 'POST',
+        mode: 'no-cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        redirect: 'follow',
+        referrerPolicy: 'no-referrer',
+        body: JSON.stringify(payload)
+      };
+    }
+
+    function fireAndForget (payload) {
+      const disH = quickH(payload);
+      if (disH === lastHash) return;
+      lastHash = disH;
+
+      if (debug) {
+        console.log(' 🤖  Sending change event with payload:', { payload, req });
+      }
+
+      return fetch(
+        '${serverUrl}/event',
+        req(payload)
+      ).then(
+        resp => {
+          if (debug) {
+            console.log(' 🤖  recieved success:', { resp });
+          }
+          return resp;
+        },
+        err => {
+          if (debug) {
+            console.log(' 🤖  recieved error:', { err });
+          }
+          return err;
+        }
+      );
+    }
+    
+    const timer = setInterval(() => {
+      fireAndForget({
+        name,
+        text: editor.innerText
+      });
+    }, 1000);
+    
+    window.cssancelConn = () => {
+      const log = debug ? console.log : () => undefined;
+
+      if (!timer) {
+        log(' 🤖  No timer found to cancel.');
+      } else {
+        clearInterval(timer);
+        log(' 🤖  Timer canceled. Goodbye.');
+      }
+    };
+    window.csstoggleDebug = () => {
+      debug = !debug;
+      console.log(' 🤖  Set debug to:', debug);
+    };
+
+    console.group(' 👥  Welcome to css_head2head');
+    console.log('Some commands you might enjoy knowing...');
+    console.log(' 🤖  cssancleConn()', '- will end the connection to the server.');
+    console.log(' 🤖  csstoggleDebug()', '- will print messages for each action this little client takes.');
+    console.log(' ⚔️ Happy battling. ');
+    console.groupEnd();
+  })()
+  `;
+}
